fcd18e99a1b8054507d83b2e1cef996f078d916f
Ren0628 <ren6nt@gmail.com>
2024-05-25 17:01:11 +0900

    ヘッダーアイコン変更

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -10,7 +10,7 @@
             @else
             <a class="navbar-brand" href="{{ url('/') }}">
             @endif
-                {{ config('app.name', 'Laravel') }}
+                レストラン
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                 <span class="navbar-toggler-icon"></span>
