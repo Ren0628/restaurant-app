fd48873ed409296cc8126a92e8e379336e6cf513
Ren0628 <ren6nt@gmail.com>
2024-05-25 17:00:56 +0900

    react機能削除

--- routes/api.php
+++ routes/api.php
@@ -24,69 +24,69 @@ use Illuminate\Support\Facades\Auth;
 */
 
 // Genre
-Route::get('genres', [ApiGenreController::class, 'index']);
-Route::post('genres/store', [ApiGenreController::class, 'store'])->middleware('auth:admin');
-Route::post('genres/update/{genre}', [ApiGenreController::class, 'update'])->middleware('auth:admin');
-Route::post('genres/delete/{genre}', [ApiGenreController::class, 'destroy'])->middleware('auth:admin');
+// Route::get('genres', [ApiGenreController::class, 'index']);
+// Route::post('genres/store', [ApiGenreController::class, 'store'])->middleware('auth:admin');
+// Route::post('genres/update/{genre}', [ApiGenreController::class, 'update'])->middleware('auth:admin');
+// Route::post('genres/delete/{genre}', [ApiGenreController::class, 'destroy'])->middleware('auth:admin');
 
-Route::get('restaurants', [ApiRestaurantController::class, 'index']);
-Route::post('restaurants/store', [ApiRestaurantController::class, 'store'])->middleware('auth:owner');
-Route::post('restaurants/update/{restaurant}', [ApiRestaurantController::class, 'update'])->middleware('auth:owner,admin');
-Route::post('restaurants/delete/{restaurant}', [ApiRestaurantController::class, 'destroy'])->middleware('auth:owner,admin');
+// Route::get('restaurants', [ApiRestaurantController::class, 'index']);
+// Route::post('restaurants/store', [ApiRestaurantController::class, 'store'])->middleware('auth:owner');
+// Route::post('restaurants/update/{restaurant}', [ApiRestaurantController::class, 'update'])->middleware('auth:owner,admin');
+// Route::post('restaurants/delete/{restaurant}', [ApiRestaurantController::class, 'destroy'])->middleware('auth:owner,admin');
 
-Route::post('restaurants/keyword', [ApiRestaurantController::class, 'searchRestaurants']);
-Route::post('restaurants/nearby', [ApiRestaurantController::class, 'nearbyRestaurants']);
+// Route::post('restaurants/keyword', [ApiRestaurantController::class, 'searchRestaurants']);
+// Route::post('restaurants/nearby', [ApiRestaurantController::class, 'nearbyRestaurants']);
 
-Route::get('menus', [ApiMenuController::class, 'index']);
-Route::post('menus/store/{restaurant}', [ApiMenuController::class, 'store'])->middleware('auth:owner,admin');
-Route::post('menus/update/{menu}', [ApiMenuController::class, 'update'])->middleware('auth:owner,admin');
-Route::post('menus/delete/{menu}', [ApiMenuController::class, 'destroy'])->middleware('auth:owner,admin');
+// Route::get('menus', [ApiMenuController::class, 'index']);
+// Route::post('menus/store/{restaurant}', [ApiMenuController::class, 'store'])->middleware('auth:owner,admin');
+// Route::post('menus/update/{menu}', [ApiMenuController::class, 'update'])->middleware('auth:owner,admin');
+// Route::post('menus/delete/{menu}', [ApiMenuController::class, 'destroy'])->middleware('auth:owner,admin');
 
-Route::get('reviews', [ApiReviewController::class, 'index']);
-Route::post('reviews/store/{restaurant}', [ApiReviewController::class, 'store'])->middleware('auth:web');
-Route::post('reviews/update/{review}', [ApiReviewController::class, 'update'])->middleware('auth:web,admin');
-Route::post('reviews/delete/{review}', [ApiReviewController::class, 'destroy'])->middleware('auth:web,admin');
+// Route::get('reviews', [ApiReviewController::class, 'index']);
+// Route::post('reviews/store/{restaurant}', [ApiReviewController::class, 'store'])->middleware('auth:web');
+// Route::post('reviews/update/{review}', [ApiReviewController::class, 'update'])->middleware('auth:web,admin');
+// Route::post('reviews/delete/{review}', [ApiReviewController::class, 'destroy'])->middleware('auth:web,admin');
 
-Route::post('isBookmark/{restaurant}', [ApiBookmarkController::class, 'isBookmark'])->middleware('auth:web');
-Route::post('bookmark/{restaurant}', [ApiBookmarkController::class, 'bookmark'])->middleware('auth:web');
+// Route::post('isBookmark/{restaurant}', [ApiBookmarkController::class, 'isBookmark'])->middleware('auth:web');
+// Route::post('bookmark/{restaurant}', [ApiBookmarkController::class, 'bookmark'])->middleware('auth:web');
 
 
-Route::get('/auth', function () {
-    if(Auth::guard('web')->check()) {
-        return response()->json([
-            'message' => 'Authenticated.',
-            'auth' => Auth::guard('web')->user(),
-            'guard' => 'user',
-        ]);
-    } elseif(Auth::guard('owner')->check()) {
-        return response()->json([
-            'message' => 'Authenticated.',
-            'auth' => Auth::guard('owner')->user(),
-            'guard' => 'owner',
-        ]);
-    } elseif(Auth::guard('admin')->check()) {
-        return response()->json([
-            'message' => 'Authenticated.',
-            'auth' => Auth::guard('admin')->user(),
-            'guard' => 'admin',
-        ]);
-    } else {
-        return response()->json([
-            'message' => 'UnAuthenticated.',
-            'auth' => '' ,
-        ]);
-    }
-});
+// Route::get('/auth', function () {
+//     if(Auth::guard('web')->check()) {
+//         return response()->json([
+//             'message' => 'Authenticated.',
+//             'auth' => Auth::guard('web')->user(),
+//             'guard' => 'user',
+//         ]);
+//     } elseif(Auth::guard('owner')->check()) {
+//         return response()->json([
+//             'message' => 'Authenticated.',
+//             'auth' => Auth::guard('owner')->user(),
+//             'guard' => 'owner',
+//         ]);
+//     } elseif(Auth::guard('admin')->check()) {
+//         return response()->json([
+//             'message' => 'Authenticated.',
+//             'auth' => Auth::guard('admin')->user(),
+//             'guard' => 'admin',
+//         ]);
+//     } else {
+//         return response()->json([
+//             'message' => 'UnAuthenticated.',
+//             'auth' => '' ,
+//         ]);
+//     }
+// });
 
-Route::post('user/register', [ApiAuthUserController::class, 'register']);
-Route::post('user/login', [ApiAuthUserController::class, 'login']);
-Route::post('user/logout', [ApiAuthUserController::class, 'logout'])->middleware('auth:web');
+// Route::post('user/register', [ApiAuthUserController::class, 'register']);
+// Route::post('user/login', [ApiAuthUserController::class, 'login']);
+// Route::post('user/logout', [ApiAuthUserController::class, 'logout'])->middleware('auth:web');
 
-Route::post('owner/register', [ApiAuthOwnerController::class, 'register']);
-Route::post('owner/login', [ApiAuthOwnerController::class, 'login']);
-Route::post('owner/logout', [ApiAuthOwnerController::class, 'logout'])->middleware('auth:owner');
+// Route::post('owner/register', [ApiAuthOwnerController::class, 'register']);
+// Route::post('owner/login', [ApiAuthOwnerController::class, 'login']);
+// Route::post('owner/logout', [ApiAuthOwnerController::class, 'logout'])->middleware('auth:owner');
 
-// Route::post('admin/register', [ApiAuthAdminController::class, 'register']);
-Route::post('admin/login', [ApiAuthAdminController::class, 'login']);
-Route::post('admin/logout', [ApiAuthAdminController::class, 'logout'])->middleware('auth:admin');
+// // Route::post('admin/register', [ApiAuthAdminController::class, 'register']);
+// Route::post('admin/login', [ApiAuthAdminController::class, 'login']);
+// Route::post('admin/logout', [ApiAuthAdminController::class, 'logout'])->middleware('auth:admin');
 
